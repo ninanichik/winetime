f2f0d1f201d28cb66ca3413edafb166efd85bfea
Nina Nychyk <testni2312@gmail.com>
2022-01-11 11:25:53 +0200

    Add Faraday gem

--- specs/connection.rb
+++ specs/connection.rb
@@ -1,13 +1,38 @@
 require 'faraday'
 require 'json'
 
-#iniitialaze
+
 connection = Faraday.new('http://localhost:4567/') do |f|
-  f.request :json # encode req bodies as JSON and automatically set the Content-Type header
-  f.response :json # decode response bodies as JSON
+  f.request :json
+  f.response :json
   # f.adapter :net_http # adds the adapter to the connection, defaults to `Faraday.default_adapter`
 end
 
 response = connection.get('/wines')
 puts response.body
+puts response.status
+
+response = connection.get('/users')
+puts response.body
+
+response = connection.post '/wine/new', { :name => 'Brunello di Montalcino',
+                                          :wine_variety => 'White',
+                                          :produced_year => '2000',
+                                          :produced_place => 'Italy' }
+puts response.body
+puts response.status
+
+response = connection.put '/wine/4', { :name => 'Muscato',
+                                         :wine_variety => 'White',
+                                         :produced_year => '2000',
+                                         :produced_place => 'Italy' }
+
+puts response.body
+puts response.status
+
+response = connection.get('/wine/4')
+puts response.body
+puts response.status
+
+response = connection.delete('/wine/5')
 puts response.status
