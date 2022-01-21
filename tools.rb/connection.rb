21e9a37d2a2967c121bcce927b39733b2be8018d
Andrii Mishchuk <amishchu@cisco.com>
2022-01-21 11:45:57 +0200

    suggestions to improve

--- tools.rb/connection.rb
+++ tools.rb/connection.rb
@@ -2,6 +2,9 @@ require 'faraday'
 require 'json'
 require 'faraday/net_http'
 require 'faraday_middleware'
+require 'securerandom'
+
+# to delete 8-12 rows
 
 #Faraday.default_adapter = :net_http
 
@@ -9,8 +12,9 @@ require 'faraday_middleware'
 # conn.request(:basic_auth, 'admin', 'admin')
 # conn.adapter  :net_http
 
+# name of class would be good to have more meaning
 class Helper
-
+  # remove host and port attributes, add just url
   def initialize(host = 'localhost', port = 4567)
     @host = host
     @port = port
@@ -22,7 +26,9 @@ class Helper
   end
 
   def get_wines
+    # remove @response from everywhere until it's getting used
     @response = @connection.get('/wines')
+    # remove puts
     puts @response
   end
 
@@ -30,6 +36,7 @@ class Helper
     @response = @connection.get('/users')
   end
 
+  # remove default values
   def add_new_wine(name = 'Brunello di Montalcino', wine_variety = 'White', produced_year = '2000', produced_place = 'Italy')
     @response = @connection.post '/wine/new' do |req|
       req.params['name'] = name
@@ -41,6 +48,7 @@ class Helper
     puts @response
   end
 
+  #remove default values
   def update_wine(id, name = 'Moet Chandon Nectar Imperial', wine_variety = 'White semi-dry', produced_year = '2010', produced_place = 'France')
     @response = @connection.put "/wine/#{id}" do |req|
       req.params['name'] = name
@@ -53,11 +61,27 @@ class Helper
 
   def get_wine_by_id(id)
     @response = @connection.get("/wine/#{id}")
+    #remove puts
     puts @response.body
   end
 
   def delete_wine(id)
     @response = @connection.delete("/wine/#{id}")
+    #remove puts
     puts @response.status
+  end
+
+  #for example
+  def random_params
+    {
+        name: SecureRandom.uuid,
+        wine_variety: SecureRandom.uuid,
+        produced_year: SecureRandom.uuid,
+        produced_place: SecureRandom.uuid
+    }
+  end
+
+  def params_null(key)
+    random_params[key] = null
   end
 end
